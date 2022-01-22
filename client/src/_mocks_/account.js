ed275ef020a7441d6ec8eb383233f8616a442f4f
sudheer <sudheer@clearglass.com>
2022-01-22 11:06:08 +0530

    feat:client

---
+++ client/src/_mocks_/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Sudheer Tripathi',
+  email: 'tripathisudheer604@gmail.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg'
+};
+
+export default account;
